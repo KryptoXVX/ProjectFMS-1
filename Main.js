0c3d1d72deb0aac4b3e7a03f3eb22c76467ea2e6
Blake <64765941+golfermatt@users.noreply.github.com>
2021-09-14 10:31:07 -0700

    add stroke, colors, and background

--- Main.js
+++ Main.js
@@ -1,10 +1,40 @@
+let x = 128;
+let y = 128;
+
+let a = 52;
+let b = 52;
+
 function setup() {
-  createCanvas(400, 400);
-  fill(0, 100, 200);
+    fill(255, 255, 255);
+    createCanvas(400, 400);
+
 }
 
 function draw() {
-    ellipse(128, 128, 100, 100)
+    background(255, 255, 255)
+
+    strokeWeight(25)
+    fill(64, 64, 255)
+    ellipse(x, y, 100, 100)
+    x--;
+    y++;
+    y++;
+
+    if (x < 0) {
+        x = 128;
+        y = 128
+    }
+
+    strokeWeight(5)
+    fill(25, 69, 25)
     rect(25, 25, 50, 50)
-    rect(200, 128, 70, 70, 15)
+
+    strokeWeight(10)
+    fill(65, 0, 0)
+    rect(a, b, 70, 70, 15)
+    a*=1.05;
+
+    if (a > 480) {
+        a = 25;
+    }
 }
