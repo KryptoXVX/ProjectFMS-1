18c48c4ee1b0779c2ce25c329b35b2924c0dbf69
Blake <64765941+golfermatt@users.noreply.github.com>
2021-09-14 10:49:50 -0700

    make shapes change direction

--- Main.js
+++ Main.js
@@ -3,6 +3,8 @@ let y = 128;
 
 let a = 52;
 let b = 52;
+let count = 0;
+let count2 = 0;
 
 function setup() {
     fill(255, 255, 255);
@@ -17,14 +19,19 @@ function draw() {
     strokeWeight(25)
     fill(64, 64, 255)
     ellipse(x, y, 100, 100)
-    x--;
+    x++
     y++;
-    y++;
+    count++;
 
-    if (x < 0) {
+    if (y > 480) {
         x = 128;
-        y = 128
+        y = 128;
+        count = 0;
     }
+
+    y *= (0.01 * count);
+
+    
 
     stroke(255, 64, 49)
     strokeWeight(5)
@@ -36,7 +43,10 @@ function draw() {
     fill(65, 0, 0)
     rect(a, b, 70, 70, 15)
     a*=1.05;
+    b++
+    count2++;
     if (a > 480) {
         a = 25;
+        b = 52;
     }
 }
