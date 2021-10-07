fe6afb4943339473b0dcd83cb90857e426604c6c
Blake <64765941+golfermatt@users.noreply.github.com>
2021-10-06 20:24:06 -0700

    start on main menu

--- Main.js
+++ Main.js
@@ -1,52 +1,20 @@
-let x = 128;
-let y = 128;
-
-let a = 52;
-let b = 52;
-let count = 0;
-let count2 = 0;
-
 function setup() {
-    fill(255, 255, 255);
     createCanvas(400, 400);
+    background(255, 255, 255);
+    puzzleButton = createButton('Puzzle Excersise');
+    puzzleButton.position(0, 0);
+    puzzleButton.mousePressed(changeBG);
 
+    aimTrainerButton = createButton("Aim Trainer Excersise");
+    aimTrainerButton.position(0, 25);
+    aimTrainerButton.mousePressed(changeBG);
+
+    pianoButton = createButton("Piano Excersise");
+    pianoButton.position(0, 50);
+    pianoButton.mousePressed(changeBG);
 }
 
-function draw() {
-    background(255, 255, 255)
-
-    stroke(0, 0, 255)
-    strokeWeight(25)
-    fill(64, 64, 255)
-    ellipse(x, y, 100, 100)
-    x++
-    y++;
-    count++;
-
-    if (y > 480) {
-        x = 128;
-        y = 128;
-        count = 0;
-    }
-
-    y *= (0.01 * count);
-
-    
-
-    stroke(255, 64, 49)
-    strokeWeight(5)
-    fill(25, 69, 25)
-    rect(25, 25, 50, 50)
-
-    stroke(23, 64, 43)
-    strokeWeight(10)
-    fill(65, 0, 0)
-    rect(a, b, 70, 70, 15)
-    a*=1.05;
-    b++
-    count2++;
-    if (a > 480) {
-        a = 25;
-        b = 52;
-    }
+function changeBG() {
+    let val = random(255);
+    background(val);
 }
