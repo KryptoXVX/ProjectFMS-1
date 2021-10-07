a022e7bc99930ce00de8966fc98921e1838454ba
golfermatt <64765941+golfermatt@users.noreply.github.com>
2021-10-07 01:31:19 -0700

    comment out stuff for later

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(400, 400);
+  background(255, 255, 255);
+  //puzzleButton = createButton('Puzzle Excersise');
+  //puzzleButton.position(0, 0);
+  //puzzleButton.mousePressed(changeBG);
+
+  //aimTrainerButton = createButton("Aim Trainer Excersise");
+  //aimTrainerButton.position(0, 25);
+  //aimTrainerButton.mousePressed(changeBG);
+
+  //pianoButton = createButton("Piano Excersise");
+  //pianoButton.position(0, 50);
+  //pianoButton.mousePressed(changeBG);
 }
 
-function draw() {
-  background(220);
+function changeBG() {
+  let val = random(255);
+  background(val);
 }
